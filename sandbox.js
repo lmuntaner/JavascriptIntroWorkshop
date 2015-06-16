eb22b21d63c0340db497d0ce4e191ace7eb95631
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2015-06-16 14:50:58 +0200

    example comment on sandbox

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+/*
+How to create multiple choice questions?
+Use the function createQuestion(question, answers)
+'question' is a string with the question
+'answers' is an array of strings with the possible answers
+*/
+
+/*
+Remember to create the function calculateScore
+when you create it, expect an array as a parameter
+the array will contain the answers to the questions
+it should return the score of the answers
+*/
+
+
+// Example:
+
+/*
+createQuestion("Is Ned Stark dead or alife?", ["dead", "alive"])
+
+function calculateScore(answers) {
+	if (answers[0] == 1) {
+		return 100
+	} else {
+		return 0
+	}
+}
+*/
